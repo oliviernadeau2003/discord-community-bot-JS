faadabc47b2a470290bd1500467f610e59495ed7
Olivier <oliviernadeau2003@gmail.com>
2025-01-06 22:36:25 -0500

    .

--- commands/utility/dispo.js
+++ commands/utility/dispo.js
@@ -17,12 +17,43 @@ module.exports = {
             option.setName('remove')
                 .setDescription('Disponibilityt to remove')
                 .setRequired(false))
+
+        // Days of week
+        .addStringOption(option =>
+            option.setName('Lundi')
+                .setDescription('Disponibility on Monday')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('Mardi')
+                .setDescription('Disponibility on Tuesday')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('Mercredi')
+                .setDescription('Disponibility on Wednesday')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('Jeudi')
+                .setDescription('Disponibility on Thursday')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('Vendredi')
+                .setDescription('Disponibility on Friday')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('Samedi')
+                .setDescription('Disponibility on Saturday')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('Dimanche')
+                .setDescription('Disponibility on Sunday')
+                .setRequired(true))
     // .addStringOption(option =>
     //     option.setName('list')
     //         .setDescription('List all disponibility')
     //         .setRequired(false))
     ,
     async execute(interaction) {
+
 
 
 
